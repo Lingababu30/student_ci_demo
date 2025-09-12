2fa1b9766400b5a192b194242464556e08f74720
Lingababu30 <lingababuaithagoni30@gmail.com>
2025-09-12 14:15:51 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 System.out.println("Hello from Pipeline CI!");
   System.out.println("to check Automation");
     System.out.println("ngriok demo");
+    System.out.println("checking webhook");
 }
 }
